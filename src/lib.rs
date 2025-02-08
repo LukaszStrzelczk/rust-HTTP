1634141e8dc3bbde79f4d9be7809d311119f2dab
Łukasz Strzelczyk <lukaszmiloblad@gmail.com>
2025-02-08 23:08:35 +0100

    added multithreading

---
+++ src/lib.rs
@@ -0,0 +1,83 @@
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0); // panics if zero
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        ThreadPool { workers, sender }
+    }
+
+    // pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+    //     if size < 1 {
+    //         return Err(PoolCreationError::InvalidConfiguration);
+    //     }
+    //     Ok(ThreadPool)
+    // }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing");
+
+            job();
+        });
+
+        Worker { id, thread }
+    }
+}
+
+#[derive(Debug)]
+pub enum PoolCreationError {
+    InvalidConfiguration,
+}
+
+impl std::fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "invalid configuration")
+    }
+}
+
+impl std::error::Error for PoolCreationError {}
